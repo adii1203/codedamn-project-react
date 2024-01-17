8bdd6de0f69b4fcbde9e787d90068789db03544d
adii1203 <aditya32ft@gmail.com>
2024-01-17 13:37:48 +0530

    draw implementation

---
+++ src/component/Canvas.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from "react";
+
+const Canvas = () => {
+  const canvasRef = useRef(null);
+  const ctx = useRef(null);
+
+  useEffect(() => {
+    const board = canvasRef.current;
+    ctx.current = board.getContext("2d");
+    ctx.current.strokeStyle = "green";
+    ctx.current.lineWidth = 2;
+    ctx.current.lineCap = "round";
+  }, []);
+
+  return (
+    <div>
+      <canvas
+        ref={(el) => (canvasRef = el)}
+        data-testid="myCanvas"
+        width="400"
+        height="400"
+        style={{
+          border: "1px solid red",
+        }}></canvas>
+    </div>
+  );
+};
+
+export default Canvas;
